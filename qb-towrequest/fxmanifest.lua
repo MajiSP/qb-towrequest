afe5cc51de35b987b19d1ca41e101118e950a6fe
Maji <30206796+PineappleOnMyPizza@users.noreply.github.com>
2023-04-29 00:06:31 -0500

    Add files via upload

---
+++ qb-towrequest/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Maji#1118'
+description 'Tow Request Script for QBCore'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua',
+}
+
+server_scripts {
+    'server/main.lua',
+}
